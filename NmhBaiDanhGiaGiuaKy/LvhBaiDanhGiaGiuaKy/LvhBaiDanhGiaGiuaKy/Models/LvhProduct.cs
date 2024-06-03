2436e1463243e2437679f1296545827ddfd56ab8
NguyenMinhHiep <166474458+29022004zaasqs@users.noreply.github.com>
2024-06-03 12:51:01 +0700

    Update NmhProduct.cs

--- NmhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/Models/LvhProduct.cs
+++ NmhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/Models/LvhProduct.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -7,31 +7,29 @@ using System.Web;
 
 namespace LvhBaiDanhGiaGiuaKy.Models
 {
-    public class LvhProduct
+    public class NmhProduct
     {
         [Key]
-        public int LvhId { get; set; }
-        [DisplayName("LVH:Họ và Tên")]
-        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
-        [StringLength(100, MinimumLength = 5, ErrorMessage = "LVH:Họ tên chứa tối thiểu 5 kí tự hoặc tối đa 100")]
+        public int NmhId { get; set; }
+        [DisplayName("NMH:Họ và Tên")]
+        [Required(ErrorMessage = "NMH: Chưa nhập dữ liệu")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "NMH:Họ tên chứa tối thiểu 5 kí tự hoặc tối đa 100")]
         [RegularExpression(@"^[A-Za-z ]{5,100}$", ErrorMessage = "Họ tên chỉ chứa ký tự viết hoa và khoảng trắng")]
-        public string LvhFullName { get; set; }
-        [DisplayName("LVH:Ảnh")]
-        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
+        public string NmhFullName { get; set; }
+        [DisplayName("NMH:Ảnh")]
+        [Required(ErrorMessage = "NMH: Chưa nhập dữ liệu")]
         [DataType(DataType.ImageUrl, ErrorMessage = "Đth:Đây không phải là URL hình ảnh hợp lệ")]
-        public string LvhImage { get; set; }
-        [DisplayName("LVH:Số lượng")]
-        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
-        [Range(1, 100, ErrorMessage = "LVH:Số lượng phải là số trong khoảng 1-100.")]
-        public int LvhQuantity { get; set; }
-        [DisplayName("LVH:Giá")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "LVH:Giá phải là số lớn hơn 0.")]
-        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
+        public string NmhImage { get; set; }
+        [DisplayName("NMH:Số lượng")]
+        [Required(ErrorMessage = "NMH: Chưa nhập dữ liệu")]
+        [Range(1, 100, ErrorMessage = "NMH:Số lượng phải là số trong khoảng 1-100.")]
+        public int NmhQuantity { get; set; }
+        [DisplayName("NMH:Giá")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "NMH:Giá phải là số lớn hơn 0.")]
+        [Required(ErrorMessage = "NMH: Chưa nhập dữ liệu")]
 
-        public decimal LvhPrice { get; set; }
-        [DisplayName("LVH:Trạng thái")]
-        
-        
-        public bool LvhIsActive { get; set; } = true;
+        public decimal NmhPrice { get; set; }
+        [DisplayName("NMH:Trạng thái")]
+        public bool NmhIsActive { get; set; } = true;
     }
 }
