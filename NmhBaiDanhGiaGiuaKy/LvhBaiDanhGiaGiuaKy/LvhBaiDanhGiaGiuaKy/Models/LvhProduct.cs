83124d0140a743372c77b537cbaa2e90f4a79bc1
NguyenMinhHiep <166474458+29022004zaasqs@users.noreply.github.com>
2024-06-03 12:52:06 +0700

    Update NmhProduct.cs

--- NmhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/Models/LvhProduct.cs
+++ NmhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/Models/LvhProduct.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace LvhBaiDanhGiaGiuaKy.Models
+namespace NmhBaiDanhGiaGiuaKy.Models
 {
     public class NmhProduct
     {
